fb25394a43f8bfee9d6e90e1543984bda85753b2
moudden1 <walid.moudden@ig2i.centralelille.fr>
2023-04-02 04:13:46 +0200

    debut identification

--- app/src/main/java/com/example/slowvf/MainActivity.java
+++ app/src/main/java/com/example/slowvf/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.slowvf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent gameActivityIntent = new Intent(MainActivity.this, Identification.class);
+        startActivity(gameActivityIntent);
+        //setContentView(R.layout.activity_main);
     }
 }
