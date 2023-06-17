5f3b458419faf02d5b3baf4159abef024937895d
saadbouhya <bouhyasaad@gmail.com>
2023-06-17 12:31:42 +0200

    Add manually the map by clicking on button

--- app/src/main/java/com/example/slowvf/View/ui/carte/CarteFragment.java
+++ app/src/main/java/com/example/slowvf/View/ui/carte/CarteFragment.java
@@ -1,18 +1,42 @@
 package com.example.slowvf.View.ui.carte;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.slowvf.R;
 import com.example.slowvf.databinding.FragmentCarteBinding;
 
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import java.io.FileInputStream;
+
 public class CarteFragment extends Fragment {
+    private MapView mapView;
+    private Button openMap;
+    private static final int SELECT_MAP_FILE = 0;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     private FragmentCarteBinding binding;
 
@@ -21,17 +45,102 @@ public class CarteFragment extends Fragment {
         CarteViewModel carteViewModel =
                 new ViewModelProvider(this).get(CarteViewModel.class);
 
-        binding = FragmentCarteBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+//        binding = FragmentCarteBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
 
-        final TextView textView = binding.textCarte;
-        carteViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        View rootView = inflater.inflate(R.layout.fragment_carte, container, false);
+
+        AndroidGraphicFactory.createInstance(getContext());
+
+        mapView = rootView.findViewById(R.id.map);
+        openMap = rootView.findViewById(R.id.openMap);
+
+        ActivityResultLauncher<Intent> contract = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result != null && result.getData() != null && result.getResultCode() == Activity.RESULT_OK) {
+                        Uri uri = result.getData().getData();
+                        openMap(uri);
+                    }
+                }
+        );
+
+        openMap.setOnClickListener(v -> {
+            Intent intent = new Intent(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ? Intent.ACTION_OPEN_DOCUMENT : Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("*/*");
+
+            contract.launch(intent);
+        });
+
+        return rootView;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void openMap(Uri uri) {
+        try {
+            /*
+             * We then make some simple adjustments, such as showing a scale bar and zoom controls.
+             */
+            mapView.getMapScaleBar().setVisible(true);
+            mapView.setBuiltInZoomControls(true);
+
+            /*
+             * To avoid redrawing all the tiles all the time, we need to set up a tile cache with an
+             * utility method.
+             */
+            TileCache tileCache = AndroidUtil.createTileCache(getContext(), "mapcache",
+                    mapView.getModel().displayModel.getTileSize(), 1f,
+                    mapView.getModel().frameBufferModel.getOverdrawFactor());
+
+            /*
+             * Now we need to set up the process of displaying a map. A map can have several layers,
+             * stacked on top of each other. A layer can be a map or some visual elements, such as
+             * markers. Here we only show a map based on a mapsforge map file. For this we need a
+             * TileRendererLayer. A TileRendererLayer needs a TileCache to hold the generated map
+             * tiles, a map file from which the tiles are generated and Rendertheme that defines the
+             * appearance of the map.
+             */
+//            FileInputStream fis = (FileInputStream) getContentResolver().openInputStream(uri);
+            FileInputStream fis = (FileInputStream) requireActivity().getContentResolver().openInputStream(uri);
+            MapDataStore mapDataStore = new MapFile(fis);
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+                    mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+            tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
+
+            /*
+             * On its own a tileRendererLayer does not know where to display the map, so we need to
+             * associate it with our mapView.
+             */
+            mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+            /*
+             * The map also needs to know which area to display and at what zoom level.
+             * Note: this map position is specific to Berlin area.
+             */
+            mapView.setCenter(new LatLong(52.517037, 13.38886));
+            mapView.setZoomLevel((byte) 12);
+        } catch (Exception e) {
+            /*
+             * In case of map file errors avoid crash, but developers should handle these cases!
+             */
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        /*
+         * Whenever your activity exits, some cleanup operations have to be performed lest your app
+         * runs out of memory.
+         */
+        mapView.destroyAll();
+        AndroidGraphicFactory.clearResourceMemoryCache();
+        super.onDestroy();
     }
 }
